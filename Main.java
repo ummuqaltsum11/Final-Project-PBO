3f94ccf2ed4c6c296d70c7a87b9036453f085754
Ummu Qaltsum <62341995+ummuqaltsum11@users.noreply.github.com>
2021-01-21 22:49:21 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,37 +22,6 @@ public class Main extends Application {
     stage.show();
   }
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
     launch(args);
   }
-//    try {
-//      DataModelTokoBuku dmtb = new DataModelTokoBuku("SQLITE");
-//      Barang brg = new Barang("Komik",27000.);
-//      Cash cs = new Cash(7, "Mikasa", "Pringsewu", "2020-12-10", brg);
-//      dmtb.addTokoBuku(cs);
-//      System.out.println("Sukses ditambahkan");
-////    launch(args);
-//    } catch (SQLException ex) {
-//      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//      System.out.println("Gagal ditambahkan");
-//    }
-//  }
-}
-
-//    try {
-//      
-//      if (null != DBConnection.getConnection("ORACLE")){
-//      System.out.println("Koneksi Sukses");
-//      } else {
-//        System.out.println("Koneksi Gagal");
-//      }
-////    launch(args);
-//    } catch (SQLException ex) {
-//      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//    }
-//  }
-//  
-//}
